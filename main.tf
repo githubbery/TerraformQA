8b56406279c6e892c994fcf2176532972a03cf52
Joe <joekirby@hotmail.co.uk>
2026-03-12 10:40:46 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstatejoe"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
@@ -25,4 +25,5 @@ resource "azurerm_resource_group" "example" {
   name     = "RG2"
   location = "westeurope"
 }
+
 
